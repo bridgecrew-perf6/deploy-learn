b51794d62fe96145e62560378add01b3de5a16a1
Shashank Kumar <shashankkumarthakur@gmail.com>
2022-01-07 18:51:20 +0530

    ✨ Add env vars

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const axios = require("axios");
-
+const { config } = require("dotenv");
+config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const newsApi =
-    "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=26e92693046341ad9d594421f6e1b21d";
+const newsApi = process.env.NEWS_API;
 
 app.get("/", async (req, res) => {
     const data = await axios.get(newsApi);
     res.json({ data: data.data.articles });
 });
 
-app.listen(3001, () => {
+app.listen(process.env.PORT, () => {
     console.log("I have started the server.");
 });
